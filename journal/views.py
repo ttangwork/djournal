c942e6294593750a14b6a967b1dfc81db8d39022
Tommy Tang <ttangwork@gmail.com>
2026-06-21 23:35:02 +1000

    full CRUD

--- journal/views.py
+++ journal/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Entry
 
 def entry_list(request):
     entries = Entry.objects.all().order_by('-created_at')
     return render(request, 'journal/entry_list.html', {'entries': entries})
 
+def entry_detail(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    return render(request, 'journal/entry_detail.html', {'entry': entry})
+
+def entry_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        Entry.objects.create(title=title, body=body)
+        return redirect('entry_list')
+    return render(request, 'journal/entry_form.html')
+
+def entry_edit(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    if request.method == 'POST':
+        entry.title = request.POST['title']
+        entry.body = request.POST['body']
+        entry.save()
+        return redirect('entry_detail', pk=entry.pk)
+    return render(request, 'journal/entry_form.html', {'entry': entry})
+
+def entry_delete(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    if request.method == 'POST':
+        entry.delete()
+        return redirect('entry_list')
+    return render(request, 'journal/entry_confirm_delete.html', {'entry': entry})
