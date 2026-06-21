59d7370f411b5430db3979c4bea00efa3198ac65
Tommy Tang <ttangwork@gmail.com>
2026-06-21 15:52:57 +1000

    added urls and templates

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Entry
 
-# Create your views here.
+def entry_list(request):
+    entries = Entry.objects.all().order_by('-created_at')
+    return render(request, 'journal/entry_list.html', {'entries': entries})
+
